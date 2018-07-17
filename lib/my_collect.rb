958681fc0cb10622dc00fe33fb7234b9310b711b
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-07-17 12:38:48 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
 
+def my_collect(array)
+  
+  i = 0
+  modified_array = [ ]
+  while i < array.length
+  
+  modified_array[i] = yield array[i]
+  i+=1
+  
+  
+  
+  end
+  modified_array
+
+end
 
